6fb4e683dc2ed1669a763aa5f972d344727c3c45
Greg Badros <badros@gmail.com>
2023-05-05 14:23:29 -0700

    ioredis apparently doesn't support PX TTL expiry on getset (maybe redis doesn't support it on that op either)

--- packages/redis/src/index.js
+++ packages/redis/src/index.js
@@ -1,45 +1,45 @@
-const EventEmitter = require('events');
-const Redis = require('ioredis');
+const EventEmitter = require("events");
+const Redis = require("ioredis");
 
 class KeyvRedis extends EventEmitter {
-	constructor(uri, options) {
-		super();
-		this.ttlSupport = true;
-		this.opts = {};
-		this.opts.dialect = 'redis';
+  constructor(uri, options) {
+    super();
+    this.ttlSupport = true;
+    this.opts = {};
+    this.opts.dialect = "redis";
 
-		if ((uri.options && uri.options.family) || (uri.options && uri.isCluster)) {
-			this.redis = uri;
-		} else {
-			options = {...(typeof uri === 'string' ? {uri} : uri), ...options};
-			this.redis = new Redis(options.uri, options);
-		}
+    if ((uri.options && uri.options.family) || (uri.options && uri.isCluster)) {
+      this.redis = uri;
+    } else {
+      options = { ...(typeof uri === "string" ? { uri } : uri), ...options };
+      this.redis = new Redis(options.uri, options);
+    }
 
-		this.redis.on('error', error => this.emit('error', error));
-	}
+    this.redis.on("error", (error) => this.emit("error", error));
+  }
 
-	_getNamespace() {
-		return `namespace:${this.namespace}`;
-	}
+  _getNamespace() {
+    return `namespace:${this.namespace}`;
+  }
 
-	get(key) {
-		return this.redis.get(key)
-			.then(value => {
-				if (value === null) {
-					return undefined;
-				}
+  get(key) {
+    return this.redis.get(key).then((value) => {
+      if (value === null) {
+        return undefined;
+      }
 
-				return value;
-			});
-	}
+      return value;
+    });
+  }
 
-	getSet(key, value, ttl) {
-		let extraArgs = [];
-		if (typeof ttl === 'number') {
-			extraArgs = ['PX', ttl];
-		}
-		return this.redis
-      .getset(key, value, ...extraArgs)
+  getSet(key, value, ttl) {
+    // let extraArgs = [];
+    // if (typeof ttl === 'number') {
+    // 	extraArgs = ['PX', ttl];
+    // }
+    // ioredis doesn't support expiry on getset
+    return this.redis
+      .getset(key, value) //, ...extraArgs)
       .then((value) => {
         if (value === null) {
           return undefined;
@@ -48,81 +48,81 @@ class KeyvRedis extends EventEmitter {
         return value;
       })
       .then((value) => {
-				this.redis.sadd(this._getNamespace(), key);
-				return value
-			});
-	}
+        this.redis.sadd(this._getNamespace(), key);
+        return value;
+      });
+  }
 
+  getMany(keys) {
+    return this.redis.mget(keys).then((rows) => rows);
+  }
 
-	getMany(keys) {
-		return this.redis.mget(keys)
-			.then(rows => rows);
-	}
+  set(key, value, ttl) {
+    if (typeof value === "undefined") {
+      return Promise.resolve(undefined);
+    }
 
-	set(key, value, ttl) {
-		if (typeof value === 'undefined') {
-			return Promise.resolve(undefined);
-		}
+    return Promise.resolve()
+      .then(() => {
+        if (typeof ttl === "number") {
+          return this.redis.set(key, value, "PX", ttl);
+        }
 
-		return Promise.resolve()
-			.then(() => {
-				if (typeof ttl === 'number') {
-					return this.redis.set(key, value, 'PX', ttl);
-				}
+        return this.redis.set(key, value);
+      })
+      .then(() => this.redis.sadd(this._getNamespace(), key))
+      .then(() => undefined);
+  }
 
-				return this.redis.set(key, value);
-			})
-			.then(() => this.redis.sadd(this._getNamespace(), key))
-			.then(() => undefined);
-	}
+  delete(key) {
+    return this.redis
+      .del(key)
+      .then((items) =>
+        this.redis.srem(this._getNamespace(), key).then(() => items > 0)
+      );
+  }
 
-	delete(key) {
-		return this.redis.del(key)
-			.then(items => this.redis.srem(this._getNamespace(), key)
-				.then(() => items > 0));
-	}
+  deleteMany(key) {
+    return this.delete(key);
+  }
 
-	deleteMany(key) {
-		return this.delete(key);
-	}
+  clear() {
+    return this.redis
+      .smembers(this._getNamespace())
+      .then((keys) => this.redis.del([...keys, this._getNamespace()]))
+      .then(() => undefined);
+  }
 
-	clear() {
-		return this.redis.smembers(this._getNamespace())
-			.then(keys => this.redis.del([...keys, this._getNamespace()]))
-			.then(() => undefined);
-	}
+  async *iterator(namespace) {
+    const scan = this.redis.scan.bind(this.redis);
+    const get = this.redis.mget.bind(this.redis);
+    async function* iterate(curs, pattern) {
+      const [cursor, keys] = await scan(curs, "MATCH", pattern);
 
-	async * iterator(namespace) {
-		const scan = this.redis.scan.bind(this.redis);
-		const get = this.redis.mget.bind(this.redis);
-		async function * iterate(curs, pattern) {
-			const [cursor, keys] = await scan(curs, 'MATCH', pattern);
+      if (keys.length > 0) {
+        const values = await get(keys);
+        for (const [i] of keys.entries()) {
+          const key = keys[i];
+          const value = values[i];
+          yield [key, value];
+        }
+      }
 
-			if (keys.length > 0) {
-				const values = await get(keys);
-				for (const [i] of keys.entries()) {
-					const key = keys[i];
-					const value = values[i];
-					yield [key, value];
-				}
-			}
+      if (cursor !== "0") {
+        yield* iterate(cursor, pattern);
+      }
+    }
 
-			if (cursor !== '0') {
-				yield * iterate(cursor, pattern);
-			}
-		}
+    yield* iterate(0, `${namespace}:*`);
+  }
 
-		yield * iterate(0, `${namespace}:*`);
-	}
+  has(key) {
+    return this.redis.exists(key).then((value) => value !== 0);
+  }
 
-	has(key) {
-		return this.redis.exists(key)
-			.then(value => value !== 0);
-	}
-
-	disconnect() {
-		return this.redis.disconnect();
-	}
+  disconnect() {
+    return this.redis.disconnect();
+  }
 }
 
 module.exports = KeyvRedis;
